d046555fde212a8bfb90213ee3e684476474fe3e
zohaib <muhammadzohaibmalik10@gmail.com>
2025-08-21 14:41:57 +0500

    feat: Add asyncHandler middleware for handling asynchronous route handlers

---
+++ src/middlewares/asyncHandler-middleware.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, RequestHandler, Response } from 'express';
+export const asyncHandler = (handler: RequestHandler): RequestHandler => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await handler(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+};
