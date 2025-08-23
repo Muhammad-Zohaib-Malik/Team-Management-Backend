e0a8a74acacd4c930ecedaf33bd33cd3cab33a91
zohaib <muhammadzohaibmalik10@gmail.com>
2025-08-23 19:50:53 +0500

    feat: Implement role seeder to initialize roles and permissions in the database

---
+++ src/seeders/role.seeder.ts
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+import connectDB from '../config/db.config.js';
+import { RoleModel } from '../models/role.permission.model.js';
+import { RolePermissions } from '../utils/role.permission.js';
+
+const seedRoles = async () => {
+  console.log('Seeding roles started...');
+
+  try {
+    await connectDB();
+
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    console.log('Clearing existing roles...');
+    await RoleModel.deleteMany({}, { session });
+
+    for (const roleName in RolePermissions) {
+      const role = roleName as keyof typeof RolePermissions;
+      const permissions = RolePermissions[role];
+
+      // Check if the role already exists
+      const existingRole = await RoleModel.findOne({ name: role }).session(
+        session
+      );
+      if (!existingRole) {
+        const newRole = new RoleModel({
+          name: role,
+          permissions: permissions
+        });
+        await newRole.save({ session });
+        console.log(`Role ${role} added with permissions.`);
+      } else {
+        console.log(`Role ${role} already exists.`);
+      }
+    }
+
+    await session.commitTransaction();
+    console.log('Transaction committed.');
+
+    await session.endSession();
+    console.log('Session ended.');
+
+    console.log('Seeding completed successfully.');
+  } catch (error) {
+    console.error('Error during seeding:', error);
+  }
+};
+
+seedRoles().catch((error) =>
+  console.error('Error running seed script:', error)
+);
