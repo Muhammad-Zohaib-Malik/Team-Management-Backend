ca63252d40fae8c21655054d9c3644a7d41bff4d
zohaib <muhammadzohaibmalik10@gmail.com>
2025-08-23 12:35:34 +0500

    feat: Add role and permission management with Role and RolePermissions models

---
+++ src/models/role.permission.model.ts
@@ -0,0 +1,31 @@
+import mongoose, { Document, Schema } from 'mongoose';
+import { PermissionType, Roles, RoleType } from '../enums/role.enum.js';
+import { RolePermissions } from '../utils/role.permission.js';
+export interface RoleDocument extends Document {
+  name: RoleType;
+  permissions: Array<PermissionType>;
+}
+
+const roleSchema = new Schema<RoleDocument>(
+  {
+    name: {
+      type: String,
+      enum: Object.values(Roles),
+      required: true,
+      unique: true
+    },
+    permissions: {
+      type: [String],
+      enum: Object.values(Permissions),
+      required: true,
+      default: function (this: RoleDocument) {
+        return RolePermissions[this.name];
+      }
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+export const RoleModel = mongoose.model<RoleDocument>('Role', roleSchema);
