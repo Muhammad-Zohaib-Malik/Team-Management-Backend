420a0d7bd24ade82b6571896e3c6d68cb5e08e76
zohaib <muhammadzohaibmalik10@gmail.com>
2025-09-07 19:11:49 +0500

    feat: Add Google OAuth client implementation

---
+++ src/lib/oauth/google.ts
@@ -0,0 +1,11 @@
+import { Google } from 'arctic';
+
+const clientId = process.env.GOOGLE_CLIENT_ID;
+const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+const redirectUri = process.env.GOOGLE_REDIRECT_URI;
+
+export const googleOAuthClient = new Google(
+  clientId,
+  clientSecret,
+  redirectUri
+);
